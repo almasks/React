0e03ceb7debaad02342c5e1ffe55820bbfc4826d
Almas K S <mailmealmasks@gmail.com>
2023-07-05 22:19:36 +0530

    setState

---
+++ src/components/Hello.js
@@ -0,0 +1,6 @@
+import React from "react";
+const Hello=()=><h1>Hello Vishwas</h1>
+// const Hello= ()=>{
+//     return React.createElement('div',null,React.createElement('h1',null,'hello'))
+// }
+export default Hello
